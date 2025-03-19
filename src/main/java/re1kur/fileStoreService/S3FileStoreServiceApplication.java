543d25162c55b54439ba4abb911aaa05c64df58e
re1kur <re1kur@yahoo.com>
2025-03-19 22:15:24 +0700

    init project

---
+++ src/main/java/re1kur/fileStoreService/S3FileStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package re1kur.fileStoreService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3FileStoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3FileStoreServiceApplication.class, args);
+	}
+
+}
